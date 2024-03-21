e51e0d461c8802175760558c5427b2f093df1fc6
Jairo0624 <alvaradogonzalezjairo@gmail.com>
2024-03-20 23:56:36 -0600

    Calculadora Operadores

---
+++ Calculadora cientifica/js/index.js
@@ -0,0 +1,32 @@
+
+let resultado = document.getElementById('resultado');
+
+    
+let asignar = (valor) => {
+    resultado.value += valor;
+}
+
+
+let calcular = () => {
+    resultado.value = eval(resultado.value);
+}
+
+let limpiar = () => {
+    resultado.value = '';
+}
+
+let calcularRaiz = () => {
+    resultado.value = Math.sqrt(eval(resultado.value));
+}
+
+let calcularPotencia = () => {
+    resultado.value = Math.pow(eval(resultado.value), 2);
+}
+
+let calcularEuler = () => {
+    resultado.value = Math.exp(eval(resultado.value));
+}
+
+let calcularPi = () => {
+    resultado.value = Math.PI;
+}
